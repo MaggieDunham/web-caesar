a69bc5aec6a9df1050d32855b349e3c9c9a9175b
Maggie Dunham <maggiedunham@Maggies-MacBook-Air.local>
2019-01-20 21:14:21 -0600

    changed a few lines

--- main.py
+++ main.py
@@ -11,29 +11,28 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-                }
-            textarea{
+                }}
+            textarea{{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
 
         <form action ="/"  method ="POST">
             <label for="rot">Rotate by:</label>
-            <input type= "text" name= "rot" value="0"/>
+            <input type= "text" name= "rot" value=0 />
             <br>
-            <textarea name="text">
-            </textarea>   
+            <textarea name="text">{0}</textarea>   
                               
             <input type="submit" value="Submit Query"/>
             
@@ -47,7 +46,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 
 @app.route("/", methods=['POST'])
@@ -55,7 +54,7 @@ def encrypt():
     rotated = int(request.form['rot'])
     message = request.form['text']
     encrypted_message = rotate_string(message, rotated)
-    #return"<<h1>" + encrypted_message +"</h1>"
-    return form.format(rotate_string(message,rotated))
+    #return"<h1>"  encrypted_message  "</h1>"
+    return form.format(encrypted_message)
    
 app.run()
